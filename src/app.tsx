3e0edeb51d21090f4822f4332f94435f5f3d278e
ziontee113 <ziontee113@gmail.com>
2023-06-17 10:48:54 +0700

    Initial Scaffolding

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+export function App() {
+  return <div className="h-screen bg-stone-900 text-white flex flex-col items-center justify-center">
+      <h1 className="text-8xl">Hello World</h1>
+  </div>;
+}
